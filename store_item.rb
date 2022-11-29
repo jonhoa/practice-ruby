907cdd0ef5719a4f6c9fefef74f64d6177d98692
Jonathan <jonitoadriano@gmail.com>
2022-11-29 14:28:45 -0600

    add att_r and att_w

--- store_item.rb
+++ store_item.rb
@@ -4,34 +4,61 @@
 #puts items
 
 # Using Classes
-class Store
-   def initialize(item_name, item_color, item_price, item_material)
-     @name = item_name
-     @color = item_color
-     @price = item_price
-     @material = item_material
-   end
+#class Store
+  # def initialize(item_name, item_color, item_price, item_material)
+   #  @name = item_name
+    # @color = item_color
+    #@price = item_price
+     #@material = item_material
+   #end
   
-   def item_name
-    @name
-   end
+   #def item_name
+   # @name
+   #end
 
-   def item_color
-     @color
-   end
+   #def item_color
+    ## @color
+   #end
 
-    def item_price
-      @price
-    end
+    #def item_price
+     # @price
+    #end
 
-    def item_material
-        @material
-    end
+    #def item_material
+     #   @material
+    #end
 
-    def info
-      puts @name + @color
-    end
+    #def info
+     # puts @name + @color
+    #end
+#end
+
+#store1 = Store.new("Crowbar", "Blue", 50, "Plastic")
+#puts store1.item_name
+
+class Store
+  attr_reader :item_name, :item_color, :item_price, :item_material
+  attr_writer :item_name
 end
 
-store1 = Store.new("Crowbar", "Blue", 50, "Plastic")
-puts store1.item_name
+def initialize(input)
+  @item_name = input[:item_name]
+end
+
+store1 = Store.new(item_name: "Crowbar", item_color: "Red", item_price: 5)
+p store1.item_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
