03700a0c06d7eb4782fddc36e985267792f93014
Jonathan <jonitoadriano@gmail.com>
2022-11-29 13:52:47 -0600

    adds three new items

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+items1 = [:name => "Whiskey", :color => "Red", :price => 50, :material => "plastic"]
+items2 = [:name => "Crowbar",:color => "Red", :price => 50, :material => "plastic"]
+items3 = [:name => "Mollusks", :color => "Red", :price => 50, :material => "plastic"]
+puts items
